4c3ee03509c43c9696687eb82d934a9b668cd66c
Minh Vuong <minhvuonguet@gmail.com>
2024-05-14 17:26:59 +0700

    update Dockerfile

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'core-api-lms',
+      name: 'Kosei web',
       script: 'npm start',
       ignore_watch: ['node_modules'],
       instances: 1,
       autorestart: true,
       env: {
-        NODE_ENV: 'prod',
+        NODE_ENV: 'production',
       },
     },
   ],
