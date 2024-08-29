cc5394136fe4e0d3bb96ca69f3cbff63c268f69f
Nguyen Anh Duong <anhduong21112001@gmail.com>
2024-08-29 11:18:00 +0700

    save localStorage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -279,8 +279,12 @@ const Header = () => {
                   <Text
                     type="body-16-regular"
                     onClick={() => {
+                      const isRegistered = localStorage.getItem("isRegistered");
+
                       if (!user?.user_id) {
                         router.push("/login");
+                      } else if (isRegistered === "true") {
+                        router.push("/exam");
                       } else {
                         router.push({
                           pathname: "/register-trial-tests",
